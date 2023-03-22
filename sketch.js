b9a1243e49f8c9dac99b2b10a9e4106ac364794c
Eli Dodd <elijahdodd@gmail.com>
2023-03-22 12:49:29 -0700

    Added bouncing and adding/removing planets
    checkBounce() is called every frame

--- sketch.js
+++ sketch.js
@@ -1,117 +1,137 @@
-let inputPath = 'sample.wav'
-//TODO: The number of planets and the bandwidth range should be inversely proportional
-let numPlanets = 3
-const variability = 1.2
-let clickAttractor = 1.1
+let numPlanets = 3;
+const variability = 1.2;
+let clickAttractor = 1.1;
 const releaseDampener = 1.5;
 const density = 10;
 const bounceDamp = 0.95;
 const bounce = true;
-let bandwidthRange = 200; //1000 is narrowest, .001 is widest. Around 50 is interesting
+let bandwidthRange = 200; // 1000 is narrowest, .001 is widest. Around 50 is interesting.
 const inputFreq = 50;
 const oscFreq = 200;
 
 const enableGravityClick = false;
 let auxInput = true;
-let inputType = 'FILE' //MIC, FILE
+let inputType = 'FILE'; // MIC, FILE
+const inputPath = 'sample.wav'
+
 
 let soundStreamStarted = false;
 let planets = [];
-var gravityClick, click;
-
+let gravityClick, click;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // Starts in the middle
+
   centerX = width / 2;
   centerY = height / 2;
+
   if (auxInput) {
     soundStream = makeSound(inputType);
   }
-  //Make new planets
-  for (let i = 0; i < numPlanets; i++) {
-    const i = new Planet(random(width), random(height), random(10, 150));
-    planets.push(i)
-  };
 
+  for (let i = 0; i < numPlanets; i++) {
+    const p = new Planet(random(width), random(height), random(10, 150));
+    planets.push(p);
+  }
 }
 
 function draw() {
   background(200);
-  // Draw a circle
 
   fill(100);
   ellipse(centerX, centerY, 24, 24);
-  // Draw every planet
-  for (planet of planets) {
+
+  for (let planet of planets) {
     planet.drawPlanet();
-    //check if the mouse is touching the planet
-    let d = dist(mouseX, mouseY, planet.position.x, planet.position.y)
+    let d = dist(mouseX, mouseY, planet.position.x, planet.position.y);
+
     if (d < planet.radius / 2 && click) {
       planet.inPlanet = true;
     } else {
       planet.inPlanet = false;
     }
 
-    //Bounce planets off walls
     if (bounce) {
-      if (planet.position.x > width | planet.position.x < 0) {
-        planet.velocity.x = -planet.velocity.x * bounceDamp
+      if (planet.position.x > width || planet.position.x < 0) {
+        planet.velocity.x = -planet.velocity.x * bounceDamp;
       }
-      if (planet.position.y > height | planet.position.y < 0) {
-        planet.velocity.y = -planet.velocity.y * bounceDamp
+      if (planet.position.y > height || planet.position.y < 0) {
+        planet.velocity.y = -planet.velocity.y * bounceDamp;
       }
     }
-
-    //Acceleration toward gravity wells
+    checkBounce()
     if (gravityClick) {
       planet.applyForce((mouseX - planet.position.x) * clickAttractor,
-        (mouseY - planet.position.y) * clickAttractor)
+        (mouseY - planet.position.y) * clickAttractor);
     }
     if (!planet.inPlanet && planet.touched) {
-      planet.applyForce(centerX - planet.position.x, centerY - planet.position.y)
-      planet.update()
+      planet.applyForce(centerX - planet.position.x, centerY - planet.position.y);
+      planet.update();
     }
-
   }
 }
 
+function addPlanet(x, y, r) {
+  const p = new Planet(x, y, r);
+  planets.push(p);
+}
+
+function removePlanet(index) {
+  if (index >= 0 && index < planets.length) {
+    if (planets[index].filter) {
+      planets[index].filter.disconnect();
+    }
+    planets.splice(index, 1);
+  }
+}
 
 function mousePressed() {
   if (enableGravityClick) {
     gravityClick = true;
   }
   click = true;
+
+if (keyIsDown(65)) { // 'a' key
+    addPlanet(mouseX, mouseY, random(10, 150));
+  } else if (keyIsDown(82)) { // 'r' key
+    let planetIndex = -1;
+    for (let i = 0; i < planets.length; i++) {
+      if (dist(mouseX, mouseY, planets[i].position.x, planets[i].position.y) < planets[i].radius / 2) {
+        planetIndex = i;
+        break;
+      }
+    }
+    if (planetIndex >= 0) {
+      removePlanet(planetIndex);
+    }
+  }
 }
 
-
 function mouseDragged() {
-  for (planet of planets) {
+  for (let planet of planets) {
     if (planet.inPlanet) {
-      planet.position.x = mouseX
-      planet.position.y = mouseY
+      planet.position.x = mouseX;
+      planet.position.y = mouseY;
     }
   }
 }
 
 function mouseReleased() {
-  for (planet of planets) {
+  for (let planet of planets) {
     if (planet.inPlanet) {
       planet.velocity.x = movedX / releaseDampener;
       planet.velocity.y = movedY / releaseDampener;
       planet.inPlanet = false;
       planet.touched = true;
-      //if we're going off of auxilliary, get whatever aux audio type ready
-      //After you release the first planet, start the loaded audio. This is for web browsers
+
       if (auxInput) {
         if (!soundStreamStarted) {
           if (inputType === 'FILE') {
-            soundStream.loop()
-            soundStreamStarted = true
+            soundStream.loop();
+            soundStreamStarted = true;
           }
         }
       } else {
-        //start the planet's own oscillator
         planet.osc.start();
       }
     }
@@ -121,10 +141,37 @@ function mouseReleased() {
   }
   click = false;
 }
+
+function checkBounce() {
+  for (let i = 0; i < planets.length; i++) {
+    for (let j = i + 1; j < planets.length; j++) {
+      let d = dist(planets[i].position.x, planets[i].position.y, planets[j].position.x, planets[j].position.y);
+      let minDist = (planets[i].radius + planets[j].radius) / 2;
+
+      if (d <= minDist) {
+        let normal = p5.Vector.sub(planets[i].position, planets[j].position).normalize();
+        let relativeVelocity = p5.Vector.sub(planets[i].velocity, planets[j].velocity);
+
+        let impulse = p5.Vector.mult(normal, -2 * planets[j].mass / (planets[i].mass + planets[j].mass) * normal.dot(relativeVelocity));
+        planets[i].velocity.add(impulse);
+
+        impulse = p5.Vector.mult(normal, 2 * planets[i].mass / (planets[i].mass + planets[j].mass) * normal.dot(relativeVelocity));
+        planets[j].velocity.add(impulse);
+
+        while (dist(planets[i].position.x, planets[i].position.y, planets[j].position.x, planets[j].position.y) <= minDist) {
+          planets[i].position.add(planets[i].velocity);
+          planets[j].position.add(planets[j].velocity);
+        }
+      }
+    }
+  }
+}
+
 function makeSound(soundtype) {
   switch (soundtype) {
     case 'FILE':
-      soundStream = loadSound(inputPath)
+      soundStream = loadSound(inputPath);
+     
       soundStream.disconnect();
       return soundStream;
     case 'MIC':
@@ -138,10 +185,11 @@ function makeSound(soundtype) {
       return soundStream;
   }
 }
+
 class Planet {
   constructor(x, y, r) {
-    this.radius = r
-    this.mass = this.radius * density
+    this.radius = r;
+    this.mass = this.radius * density;
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
@@ -150,17 +198,16 @@ class Planet {
     this.blue = random(255);
     this.alpha = random(200, 255);
 
-    //Oscillator setup
     if (!auxInput) {
       this.osc = new p5.TriOsc();
     } else {
       this.filter = new p5.BandPass();
       this.filter.res(bandwidthRange);
-      this.filter.process(soundStream)
+      this.filter.process(soundStream);
     }
     this.inPlanet = false;
     this.touched = false;
-    this.momentum = this.mass * this.velocity
+    this.momentum = this.mass * this.velocity;
   }
 
   drawPlanet() {
@@ -170,33 +217,25 @@ class Planet {
   }
 
   update() {
-    // Velocity changes according to acceleration
     this.velocity.add(this.acceleration);
-    // Position changes by velocity
     this.position.add(this.velocity);
-    // We must clear acceleration each frame
     this.acceleration.mult(0);
-    if (auxInput) {
-      this.freq = this.radius * inputFreq * mag(variability * (this.velocity.x), variability * (this.velocity.y)) / this.radius
-    } else {
-      this.freq = this.radius * oscFreq * mag(variability * (this.velocity.x), variability * (this.velocity.y)) / this.radius
-    }
-    // If there's no input, synthesize. If there is an input, filter input
-    if (auxInput) {
-      this.filter.set(this.freq)
-    } else {
-      this.osc.freq(this.freq)
-      this.osc.amp(this.momentum);
 
-    };
+    if (auxInput) {
+      this.freq = this.radius * inputFreq * mag(variability * (this.velocity.x), variability * (this.velocity.y)) / this.radius;
+    } else {
+      this.freq = this.radius * oscFreq * mag(variability * (this.velocity.x), variability * (this.velocity.y)) / this.radius;
+    }
+
+    if (auxInput) {
+      this.filter.set(this.freq);
+    } else {
+      this.osc.freq(this.freq);
+      this.osc.amp(this.momentum);
+    }
   }
 
   applyForce(xComp, yComp) {
-    this.acceleration.add(xComp / this.mass, yComp / this.mass)
+    this.acceleration.add(xComp / this.mass, yComp / this.mass);
   }
 }
-
-
-//TODO: Universe gravity body physics - Planets bounce off each other
-
-//TODO: button to add new planets
